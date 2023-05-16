263c61ed1da4566cbd0a29980138e0317de24f6e
Nadezhda-070283 <nsu_07@mail.ru>
2023-05-16 10:44:03 +0300

    Реализовано четвертое задание

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,31 @@
 function Student(name, gender, age) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+new Student();
+new Student();
+new Student();
+
+
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function(...marks) {
+	if (this.marks) {
+		for (mark of marks)
+			this.marks.push(mark);
+
+	}
 }
 
-Student.prototype.getAverage = function () {
-  
-}
-
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.getAverage = function() {
+	if (this.marks === undefined || this.marks.length === 0) {
+		return 0;
+	} else {
+		return this.marks.reduce((a, v) => a + v / this.marks.length, 0);
+	}
 }
