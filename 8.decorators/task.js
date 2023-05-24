676104bab376a09924b5ee592de0a29ee289996d
Nadezhda-070283 <nsu_07@mail.ru>
2023-05-24 15:26:39 +0300

    Реализовано восьмое задание

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,44 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+  let cache = [];
+ 
+  function wrapper(...args) {
+    const hash = md5(args);
+    let objectInCache  = cache.findIndex((item) => item.hash == hash);
+    if (objectInCache !== -1) {
+      console.log("Из кэша: " + cache[objectInCache].value);
+      return "Из кэша: " + cache[objectInCache].value;
+    }
+ 
+    let result = func(...args);
+    cache.push({
+      'hash': hash,
+      'value': result
+    });
+    if (cache.length > 5) {
+      cache.shift();
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  }
+  return wrapper;
 }
 
 //Задача № 2
-function debounceDecoratorNew(func, delay) {
-  
+
+  function debounceDecoratorNew(func, delay) {
+    let timeoutId = null;
+    wrapper.count=0;
+    wrapper.allCount=0;
+    function wrapper(...args){
+      wrapper.allCount++;
+      if (timeoutId === null) {
+        func(...args);
+        wrapper.count++;
+      }
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => {wrapper.count++; func(...args)}, delay);
+      
+    }
+    return wrapper;
 }
